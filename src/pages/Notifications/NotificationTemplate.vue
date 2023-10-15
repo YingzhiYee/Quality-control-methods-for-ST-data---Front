ed7271e983eb39a0a68bd4754567122ec03174e4
Shamman626 <m15302938395@163.com>
2023-10-15 15:47:53 +0800

    项目初始化

---
+++ src/pages/Notifications/NotificationTemplate.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Welcome to <b>White Dashboard</b> - a beautiful freebie for every web
+    developer.
+  </div>
+</template>
+
+<script>
+export default {
+  name: "notification-template",
+};
+</script>
