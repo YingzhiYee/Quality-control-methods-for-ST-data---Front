91a967243cbf64b5966fc7ec53f27ce2093dc57d
Shamman626 <m15302938395@163.com>
2023-10-15 16:30:52 +0800

    删除无用页面

--- src/pages/Layout/TopNavbar.vue
+++ src/pages/Layout/TopNavbar.vue
@@ -69,51 +69,51 @@
             />
           </modal>
 
-          <drop-down>
-            <a
-              href="javascript:void(0)"
-              data-toggle="dropdown"
-              class="dropdown-toggle nav-link"
-            >
-              <div class="notification d-none d-lg-block d-xl-block"></div>
+<!--          <drop-down>-->
+<!--            <a-->
+<!--              href="javascript:void(0)"-->
+<!--              data-toggle="dropdown"-->
+<!--              class="dropdown-toggle nav-link"-->
+<!--            >-->
+<!--              <div class="notification d-none d-lg-block d-xl-block"></div>-->
 
-              <i class="tim-icons icon-sound-wave"></i>
+<!--              <i class="tim-icons icon-sound-wave"></i>-->
 
-              <p class="d-lg-none text-left">Notifications</p>
-            </a>
+<!--              <p class="d-lg-none text-left">Notifications</p>-->
+<!--            </a>-->
 
-            <ul class="dropdown-menu dropdown-menu-right dropdown-navbar">
-              <li class="nav-link">
-                <a href="#" class="nav-item dropdown-item">
-                  Mike John responded to your email
-                </a>
-              </li>
+<!--            <ul class="dropdown-menu dropdown-menu-right dropdown-navbar">-->
+<!--              <li class="nav-link">-->
+<!--                <a href="#" class="nav-item dropdown-item">-->
+<!--                  Mike John responded to your email-->
+<!--                </a>-->
+<!--              </li>-->
 
-              <li class="nav-link">
-                <a href="javascript:void(0)" class="nav-item dropdown-item">
-                  You have 5 more tasks
-                </a>
-              </li>
+<!--              <li class="nav-link">-->
+<!--                <a href="javascript:void(0)" class="nav-item dropdown-item">-->
+<!--                  You have 5 more tasks-->
+<!--                </a>-->
+<!--              </li>-->
 
-              <li class="nav-link">
-                <a href="javascript:void(0)" class="nav-item dropdown-item">
-                  Your friend Michael is in town
-                </a>
-              </li>
+<!--              <li class="nav-link">-->
+<!--                <a href="javascript:void(0)" class="nav-item dropdown-item">-->
+<!--                  Your friend Michael is in town-->
+<!--                </a>-->
+<!--              </li>-->
 
-              <li class="nav-link">
-                <a href="javascript:void(0)" class="nav-item dropdown-item">
-                  Another notification
-                </a>
-              </li>
+<!--              <li class="nav-link">-->
+<!--                <a href="javascript:void(0)" class="nav-item dropdown-item">-->
+<!--                  Another notification-->
+<!--                </a>-->
+<!--              </li>-->
 
-              <li class="nav-link">
-                <a href="javascript:void(0)" class="nav-item dropdown-item">
-                  Another one
-                </a>
-              </li>
-            </ul>
-          </drop-down>
+<!--              <li class="nav-link">-->
+<!--                <a href="javascript:void(0)" class="nav-item dropdown-item">-->
+<!--                  Another one-->
+<!--                </a>-->
+<!--              </li>-->
+<!--            </ul>-->
+<!--          </drop-down>-->
 
           <drop-down>
             <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
@@ -127,11 +127,11 @@
             </a>
 
             <ul class="dropdown-menu dropdown-navbar">
-              <li class="nav-link">
-                <a href="javascript:void(0)" class="nav-item dropdown-item">
-                  Profile
-                </a>
-              </li>
+<!--              <li class="nav-link">-->
+<!--                <a href="javascript:void(0)" class="nav-item dropdown-item">-->
+<!--                  Profile-->
+<!--                </a>-->
+<!--              </li>-->
 
               <li class="nav-link">
                 <a href="javascript:void(0)" class="nav-item dropdown-item">
--- src/router.js
+++ src/router.js
@@ -1,13 +1,8 @@
 import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
 
 import Dashboard from "@/pages/Dashboard.vue";
-// import Icons from "@/pages/Icons.vue";
-// import Maps from "@/pages/Maps.vue";
-// import Notifications from "@/pages/Notifications.vue";
 import UserProfile from "@/pages/UserProfile.vue";
 import TableList from "@/pages/TableList.vue";
-// import Typography from "@/pages/Typography.vue";
-import UpgradeToPRO from "@/pages/UpgradeToPRO.vue";
 // import Signup from "@/pages/Signup.vue";
 // import Signin from "@/pages/Signin.vue";
 
@@ -32,22 +27,6 @@ const routes = [
       //   name: "Signup",
       //   component: Signup,
       // },
-
-      // {
-      //   path: "icons",
-      //   name: "Icons",
-      //   component: Icons,
-      // },
-      // {
-      //   path: "maps",
-      //   name: "Maps",
-      //   component: Maps,
-      // },
-      // {
-      //   path: "notifications",
-      //   name: "Notifications",
-      //   component: Notifications,
-      // },
       {
         path: "user",
         name: "User Profile",
@@ -57,16 +36,6 @@ const routes = [
         path: "table",
         name: "Table List",
         component: TableList,
-      },
-      // {
-      //   path: "typography",
-      //   name: "Typography",
-      //   component: Typography,
-      // },
-      {
-        path: "upgradeToPro",
-        name: "Upgrade to PRO",
-        component: UpgradeToPRO,
       },
     ],
   },
